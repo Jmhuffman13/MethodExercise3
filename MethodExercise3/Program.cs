54c0f5e876ea87c944725c5fcbc79f1e48d4bff9
John Huffman <jmhuffman37@gmail.com>
2020-06-19 13:33:24 -0500

    finished the user questions

--- MethodExercise3/Program.cs
+++ MethodExercise3/Program.cs
@@ -6,7 +6,19 @@ namespace MethodExercise3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"What is your favorite color?");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine($"Where are you from?");
+            var userLocation = Console.ReadLine();
+
+            Console.WriteLine($"How old are you?");
+            var userAge = Console.ReadLine();
+
+          
         }
     }
 }
