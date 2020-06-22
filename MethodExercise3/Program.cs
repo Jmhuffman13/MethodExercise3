bdeb41d7c10231e9558931070ae63a0b398dcdcb
John Huffman <jmhuffman37@gmail.com>
2020-06-22 08:39:11 -0500

    ready to upload

--- MethodExercise3/Program.cs
+++ MethodExercise3/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MethodExercise3
 {
     class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            var answer = num1 * num2 * num3;
+            return answer;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            var answer = num1 / num2;
+            return answer;
+        }
         static void Main(string[] args)
         {
+          
             Console.WriteLine($"What is your name?");
             var userName = Console.ReadLine();
 
@@ -18,7 +36,18 @@ namespace MethodExercise3
             Console.WriteLine($"How old are you?");
             var userAge = Console.ReadLine();
 
-          
+            Console.WriteLine($"Hello my name is {userName}, I am {userAge}. My favorite color is {favColor} and I am from {userLocation}.");
+
+
+
+            
+
+            
+
+             
+
+            
+            
         }
     }
 }
